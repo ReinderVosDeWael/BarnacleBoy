86f7c20d7aed23ed3fece929a113ab753df53fa6
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2023-01-08 16:20:00 +0100

    Add user journeys

---
+++ barnacleboy/mermaid/user_journey.py
@@ -0,0 +1,86 @@
+""" Mermaid diagrams for User Journeys. """
+import dataclasses
+from typing import Optional, List
+
+from barnacleboy.mermaid.base import MermaidBase
+
+
+@dataclasses.dataclass
+class Task:
+    """A task in a user journey."""
+
+    description: str
+    rating: int
+    people: List[str]
+
+    def __str__(self):
+        """Get a string representation of the object."""
+        return f"{self.description}: {self.rating}: {', '.join(self.people)}"
+
+
+class Section:
+    """A section in a user journey."""
+
+    def __init__(self, title: str, tasks: Optional[List[Task]] = None):
+        """Initialize a section.
+
+        Args:
+            title: The title of the section.
+            tasks: A list of tasks in the section.
+        """
+        self.title = title
+        self.tasks = tasks if tasks else []
+
+    def add_task(self, description, rating, people) -> None:
+        """Append a task to the section.
+
+        Args:
+            description: The description of the task.
+            rating: The rating of the task.
+            people: A list of people who are responsible for the task.
+        """
+        self.tasks.append(Task(description, rating, people))
+
+    def get_section_string(self) -> str:
+        """Get a string representation of the object."""
+        output_string = f"section {self.title}\n"
+        for task in self.tasks:
+            output_string += f"{task}\n"
+        return output_string
+
+    def __str__(self) -> str:
+        return self.get_section_string()
+
+
+class UserJourney(MermaidBase):
+    """A user journey model."""
+
+    def __init__(self, title: str, sections: Optional[List[Section]] = None):
+        """Initialize a user journey.
+
+        Args:
+            title: The title of the user journey.
+            sections: A list of sections in the user journey.
+        """
+        self.title = title
+        self.sections = sections if sections else []
+
+    def add_section(self, title: str, tasks: Optional[List[Task]] = None):
+        """Add a section to the user journey.
+
+        Args:
+            title: The title of the section.
+            tasks: A list of tasks in the section.
+        """
+        self.sections.append(Section(title, tasks))
+
+    def get_user_journey_string(self):
+        """Get a string representation of the object."""
+        output_string = "journey\n"
+        output_string += f"title {self.title}\n"
+        for section in self.sections:
+            output_string += str(section)
+        return output_string
+
+    def __str__(self):
+        return self.get_user_journey_string()
---
+++ tests/test_user_journey.py
@@ -0,0 +1,39 @@
+from barnacleboy.mermaid.user_journey import Task, Section, UserJourney
+
+
+def test_task():
+    """Test the task class."""
+    task = Task("Use the force", 5, ["Leia", "Luke"])
+
+    assert task.description == "Use the force"
+    assert task.rating == 5
+    assert task.people == ["Leia", "Luke"]
+
+    assert str(task) == "Use the force: 5: Leia, Luke"
+
+
+def test_section():
+    """Test the section class."""
+    section = Section("Return of the Jedi")
+
+    section.add_task("Use the force", 5, ["Leia", "Luke"])
+
+    assert section.title == "Return of the Jedi"
+    assert section.tasks[0] == Task("Use the force", 5, ["Leia", "Luke"])
+
+    assert str(section) == "section Return of the Jedi\nUse the force: 5: Leia, Luke\n"
+
+
+def test_user_journey():
+    """Test the user journey class."""
+    user_journey = UserJourney("Original Trilogy")
+
+    user_journey.add_section("Return of the Jedi")
+    user_journey.sections[0].add_task("Use the force", 5, ["Leia", "Luke"])
+
+    assert user_journey.title == "Original Trilogy"
+
+    assert (
+        str(user_journey)
+        == "journey\ntitle Original Trilogy\nsection Return of the Jedi\nUse the force: 5: Leia, Luke\n"
+    )
